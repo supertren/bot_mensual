1152f4dfad0f9f3c80fefcd0a89e8db6cc6e49db
supertren <supertren@hotmail.com>
2025-05-13 01:32:32 +0200

    Initial commit bot_mensual

--- bot_mensual.py
+++ bot_mensual.py
@@ -1,14 +1,15 @@
 import datetime
 import os
 from telegram import Bot
+import asyncio
 
-def enviar_mensaje(bot, chat_id):
+async def enviar_mensaje(bot, chat_id):
     ahora = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     mensaje = f"📅 Aviso programado: Hoy es {ahora}"
-    bot.send_message(chat_id=chat_id, text=mensaje)
+    await bot.send_message(chat_id=chat_id, text=mensaje)
     print(f"Mensaje enviado correctamente a las {ahora}")
 
-if __name__ == '__main__':
+async def main():
     token = os.getenv("TELEGRAM_TOKEN")
     chat_id = int(os.getenv("TELEGRAM_CHAT_ID"))
     dias_aviso = os.getenv("DIAS_AVISO", "1").split(",")  # Ej: '1,15,28' → ['1', '15', '28']
@@ -18,6 +19,10 @@ if __name__ == '__main__':
     if str(hoy) in dias_aviso:
         print(f"✅ Hoy es {hoy}, enviando aviso...")
         bot = Bot(token=token)
-        enviar_mensaje(bot, chat_id)
+        await enviar_mensaje(bot, chat_id)
     else:
         print(f"🔕 Hoy es {hoy}, no está en la lista de días configurados ({dias_aviso}). No se envía nada.")
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
